f08bb7d318e4f55f5f46b274092c3f8ea43f1f0f
DeshuiYu <dyu3@berklee.edu>
2024-10-18 09:34:13 -0400

    Main Menu Updated

---
+++ MidtermProject/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public TMP_Text pressToPlayText;
+
+    void Start()
+    {
+        StartCoroutine(BlinkText());
+    }
+
+    void Update()
+    {
+        
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+            
+        {
+            
+            
+            LoadGameScene();
+        }
+    }
+
+    IEnumerator BlinkText()
+    {
+        while (true)
+            
+        {
+            
+            pressToPlayText.enabled = !pressToPlayText.enabled;
+            
+            
+            yield return new WaitForSeconds(0.7f); 
+        }
+    }
+
+    void LoadGameScene()
+    {
+        
+        
+        SceneManager.LoadScene("GameScene");
+    }
+}
